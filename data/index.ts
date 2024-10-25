55d0b1ed3a65dbfe98286237e4279e0178fb87ae
Äbdul Mueed Shahbaz <abdulmueedshahbaz@gmail.com>
2024-10-25 23:50:25 +0500

    add java specific information

--- data/index.ts
+++ data/index.ts
@@ -6,7 +6,7 @@ const userData: IData = {
 		name: 'Abdul Mueed Shahbaz',
 		phoneNumber: '+923234942960',
 		// location: 'Berlin, Germany',
-		website: 'abdulsdevfolio.vercel.app/#/',
+		website: 'abdulmueed.dev',
 		email: 'abdulmueedshahbaz@gmail.com',
 		linkedIn: 'linkedin.com/in/-abdulmueed',
 		github: 'github.com/abdul-mueed-shz'
@@ -24,26 +24,28 @@ const userData: IData = {
 		{
 			id: uuidv4(),
 			name: 'Programming languages',
-			skill: ['JavaScript', 'Python']
+			skill: ['Java', 'Python', 'JavaScript']
 		},
 		{
 			id: uuidv4(),
 			name: 'Technologies',
 			skill: [
-				'Git',
-				'HTML',
-				'CSS',
-				'React',
-				'Vue.js',
-				'Django',
-				'Redux Toolkit',
-				'Vuex',
-				'Pinia',
-				'Tailwind CSS',
-				'Quasar',
+				'Spring Boot',
+				'Spring Security',
+				'Hibernate',
+				'Apache Ignite',
+				'Microservices',
+				'Kafka',
+				'PostgreSQL',
 				'MySQL',
-				'Web Sockets',
-				'REST APIs'
+				'JUnit',
+				'Keycloak',
+				'Maven',
+				'Docker',
+				'Git',
+				'Vue.js',
+				'React.js',
+				'Django'
 			]
 		},
 		{
@@ -55,24 +57,25 @@ const userData: IData = {
 	experience: [
 		{
 			id: uuidv4(),
-			designation: 'Software Engineer II',
+			designation: 'Senior Software Engineer',
 			company: 'Redmath',
 			companyLink: 'redmath.com',
-			location: 'Amsterdam, Netherlands',
+			location: 'Lahore, Pakistan | On-Site',
 			duration: 'Aug 2022 - Present',
 			tasks: [
-				`Designed and developed headless reusable components in React JS ,and Vue JS, enhancing code maintainability and flexibility through the implementation of <strong>Design patterns.</strong>`,
-				`Elevated code quality and maintainability by refactoring component code, utilizing composables, custom hooks, and directives.`,
-				`Engineered the Paper Generation Feature using Vue.js, Vuex, and Django, enriching Redmath's Ed-Tech product arsenal with enhanced capabilities.`,
-				`Collaborated with cross-functional teams, translating stakeholder requirements into actionable development plans for successful project outcomes.`
+				`Led the development of backend services using <strong>Java</strong> and <strong>Spring Boot</strong>, creating reusable, scalable microservices that enhanced system reliability and optimized resource usage.`,
+				`Implemented design patterns and best practices to develop robust, reusable Java-based APIs, streamlining data processing and reducing response times by 30%.`,
+				`Architected and managed RESTful APIs using Spring Boot, integrating with databases and external services to provide seamless end-user experiences.`,
+				`Collaborated with cross-functional teams to define requirements and refine project deliverables, ensuring alignment with business objectives and technical feasibility.`,
+				`Utilized Docker to containerize and orchestrate Java services, supporting scalable deployment and enhancing application stability.`
 			]
 		},
 		{
 			id: uuidv4(),
-			designation: 'Full Stack Software Engineer (React/Vue/Django)',
+			designation: 'Software Engineer',
 			company: 'Ez Bridge',
 			companyLink: 'ezbridge.io',
-			location: 'USA | Remote',
+			location: 'San Jose, USA | Remote',
 			duration: 'June 2021 - Aug 2022',
 			tasks: [
 				`Implemented <strong>Refresh Token Rotation based Authentication</strong> for enhanced API security in Django Rest Framework, demonstrating commitment to data confidentiality and reducing the risk of data breaches.`,
@@ -86,7 +89,7 @@ const userData: IData = {
 			designation: 'Fellow Game Developer',
 			company: 'Mindstorm Studios',
 			companyLink: 'mindstormstudios.com',
-			location: 'Pakistan | Hybrid',
+			location: 'Lahore, Pakistan | Hybrid',
 			duration: 'July 2021 - Sep 2021',
 			tasks: [
 				`Innovated game mechanics with a dynamic power-up system, introducing 5 distinct abilities like <strong>Speed Boosts</strong>, and <strong>Shields</strong>, elevating gameplay depth and engagement.`,
@@ -99,25 +102,25 @@ const userData: IData = {
 	projects: [
 		{
 			id: uuidv4(),
-			name: `<a href="https://github.com/Abdul-Mueed-Shahbaz/Auth-Microservice">Authentication Microservice</a>`,
-			desc: `Implemented <strong>Refresh Token Rotation Authentication System</strong> with <strong>Hacker Detection</strong> for improved security and lower risk of token leakage using <strong>Django Rest Framework, MySQL, and Django Simple JWT.</strong> Facilitated <strong>Coupled Authentication</strong> with other applications, enabling secure integration by
-			utilizing redirect URIs and URL parameters.`
+			name: `<a href="https://github.com/abdul-mueed-shz/ServiceSphere">ServiceSphere - E-Commerce Application</a>`,
+			desc: `Developed a comprehensive e-commerce platform using <strong>Spring Boot</strong> and a <strong>microservices architecture</strong>. Features include <strong>Eureka</strong> for service discovery, <strong>Config Server</strong> for externalized configuration, and <strong>Spring Cloud Gateway</strong> for API routing. Integrated <strong>Keycloak</strong> for secure authentication, and utilized <strong>Docker</strong> to manage Kafka, Zookeeper, and Postgres services.`
 		},
 		{
 			id: uuidv4(),
-			name: `<a href="https://github.com/Abdul-Mueed-Shahbaz/Secure_Channels-Hub">Authenticated Websockets Server</a>`,
-			desc: `Developed a robust <strong>real-time communication system</strong> using <strong>Django Channels</strong> and <strong>Vue.js</strong>. Features include secure <strong>JWT authentication</strong>, <strong>RESTful user authentication APIs</strong>, <strong>WebSocket integration</strong>, proactive <strong>heartbeat functionality</strong>, and <strong>Redis</strong> for caching and session management.`
+			name: `<a href="https://github.com/abdul-mueed-shz/Enrollix">Enrollix - Student Management System</a>`,
+			desc: `Built <strong>Enrollix</strong>, a robust student management system leveraging <strong>Spring Boot</strong> and <strong>Apache Ignite</strong> for scalable data handling. The platform supports <strong>student registration</strong>, <strong>course registration</strong>, and <strong>course assignments</strong>, ensuring secure and efficient management through modular APIs.`
 		},
 		{
 			id: uuidv4(),
-			name: `<a href="https://github.com/Abdul-Mueed-Shahbaz/GlowBeyond">GlowBeyond</a>`,
-			desc: `Developed a <strong>robust</strong> e-commerce platform, <strong>GlowBeyond</strong>, with <strong>Vue.js</strong> frontend and <strong>Django Rest Framework</strong> backend, integrating with <strong>auth-application</strong> for <strong>secure login</strong> and <strong>JWT token authentication</strong>. Features include showcasing <strong>latest products</strong>, categorization, detailed product pages with <strong>add-to-cart functionality</strong>, <strong>cart management</strong>, and <strong>notifications</strong>.`
+			name: `<a href="https://github.com/abdul-mueed-shz/spring-blog">AuthoBlogs - Authenticated Blogs Application</a>`,
+			desc: `Developed <strong>AuthoBlogs</strong>, a secure blog management system using <strong>Spring Boot</strong>, <strong>Spring Security</strong>, and <strong>MySQL</strong>. The platform features robust <strong>user authentication</strong>, <strong>CRUD operations</strong>, and ensures secure data management through strict access control policies.`
 		}
 	],
+
 	accomplishments: [
+		`Demonstrated Java proficiency through <a href="https://www.coursera.org/account/accomplishments/verify/FX2DYFPCRJPS">Introduction to Java</a> certification, showcasing foundational skills and a commitment to Java development.`,
 		`Mastered React intricacies with <a href="https://coursera.org/share/96a67932b7b57cd7131287028cb5aae1">Meta-React Advanced</a> certification.`,
 		`Acquired foundational knowledge of React with <a href="https://www.coursera.org/account/accomplishments/verify/FJ2S3UCQ65RN?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course">Meta-React Basics</a> certification.`,
-		`Demonstrated Python proficiency through <a href="https://www.credly.com/badges/99e03eb7-cbb0-4b83-bad8-25d880517b10?source=linked_in_profile">MTA: Introduction to Programming Using Python</a> certification, leveraging diverse resources.`,
 		`Established a notable presence on <a href="https://github.com/Abdul-Mueed-Shahbaz">GitHub</a> with diverse JavaScript technology contributions, collectively amassing over <strong>94</strong> stars for my projects.`
 	]
 };
