91b22ab40c5fe3956deb4dc5b73cc3890d612163
Äbdul Mueed Shahbaz <abdulmueedshahbaz@gmail.com>
2024-10-25 23:59:26 +0500

    readjust for python focused cv

--- data/index.ts
+++ data/index.ts
@@ -58,26 +58,26 @@ const userData: IData = {
 			designation: 'Software Engineer II',
 			company: 'Redmath',
 			companyLink: 'redmath.com',
-			location: 'Amsterdam, Netherlands',
+			location: 'Lahore, Pakistan',
 			duration: 'Aug 2022 - Present',
 			tasks: [
-				`Designed and developed headless reusable components in React JS ,and Vue JS, enhancing code maintainability and flexibility through the implementation of <strong>Design patterns.</strong>`,
+				`Designed and developed headless reusable components in <strong>Vue JS</strong> and <strong>React JS</strong>, enhancing code maintainability and flexibility through the implementation of <strong>Design patterns</strong>.`,
 				`Elevated code quality and maintainability by refactoring component code, utilizing composables, custom hooks, and directives.`,
-				`Engineered the Paper Generation Feature using Vue.js, Vuex, and Django, enriching Redmath's Ed-Tech product arsenal with enhanced capabilities.`,
+				`Handled microservice architecture in <strong>Spring Boot</strong> and <strong>Java</strong>, ensuring robustness and scalability of backend services.`,
 				`Collaborated with cross-functional teams, translating stakeholder requirements into actionable development plans for successful project outcomes.`
 			]
 		},
 		{
 			id: uuidv4(),
-			designation: 'Full Stack Software Engineer (React/Vue/Django)',
+			designation: 'Full Stack Software Engineer (Vue/Django)',
 			company: 'Ez Bridge',
 			companyLink: 'ezbridge.io',
 			location: 'USA | Remote',
 			duration: 'June 2021 - Aug 2022',
 			tasks: [
-				`Implemented <strong>Refresh Token Rotation based Authentication</strong> for enhanced API security in Django Rest Framework, demonstrating commitment to data confidentiality and reducing the risk of data breaches.`,
-				`Integrated APIs from Google, Facebook, and Instagram into the platform to enhance influencer <strong>Profile Ranking Algorithms</strong>, thereby providing brands with <strong>Advanced Analytics</strong> on influencer reach and engagement.`,
-				`Designed reusable UI components in Vue.js with the <strong>Decorator Pattern</strong>, improving code maintainability and development efficiency across multiple projects.`,
+				`Implemented <strong>Refresh Token Rotation based Authentication</strong> for enhanced API security in <strong>Django Rest Framework</strong>, demonstrating commitment to data confidentiality and reducing the risk of data breaches.`,
+				`Integrated APIs from Google, Facebook, and Instagram into the platform to enhance influencer <strong>Profile Ranking Algorithms</strong>, providing brands with <strong>Advanced Analytics</strong> on influencer reach and engagement.`,
+				`Designed reusable UI components in <strong>Vue.js</strong> with the <strong>Decorator Pattern</strong>, improving code maintainability and development efficiency across multiple projects.`,
 				`Enhanced the application by implementing robust Database-based, Text-based, and Request <strong>Logging</strong> mechanisms to mitigate developmental errors and streamline production debugging processes.`
 			]
 		},
@@ -89,7 +89,7 @@ const userData: IData = {
 			location: 'Pakistan | Hybrid',
 			duration: 'July 2021 - Sep 2021',
 			tasks: [
-				`Innovated game mechanics with a dynamic power-up system, introducing 5 distinct abilities like <strong>Speed Boosts</strong>, and <strong>Shields</strong>, elevating gameplay depth and engagement.`,
+				`Innovated game mechanics with a dynamic power-up system, introducing 5 distinct abilities like <strong>Speed Boosts</strong> and <strong>Shields</strong>, elevating gameplay depth and engagement.`,
 				`Implemented comprehensive solutions for <strong>20</strong> environmental challenges and rectified <strong>10</strong> UI bugs, refining user interaction and level design in the runner game.`,
 				`Enhanced the game's mechanics by refining running, jumping, and animation systems, resulting in a seamless gaming experience and heightened player satisfaction.`
 			]
@@ -99,19 +99,18 @@ const userData: IData = {
 	projects: [
 		{
 			id: uuidv4(),
-			name: `<a href="https://github.com/Abdul-Mueed-Shahbaz/Auth-Microservice">Authentication Microservice</a>`,
-			desc: `Implemented <strong>Refresh Token Rotation Authentication System</strong> with <strong>Hacker Detection</strong> for improved security and lower risk of token leakage using <strong>Django Rest Framework, MySQL, and Django Simple JWT.</strong> Facilitated <strong>Coupled Authentication</strong> with other applications, enabling secure integration by
-			utilizing redirect URIs and URL parameters.`
+			name: `<a href="https://github.com/abdul-mueed-shz/SecureFlex">SecureFlex</a>`,
+			desc: `Implemented <strong>Refresh Token Rotation Authentication System</strong> with <strong>Hacker Detection</strong> for enhanced security and reduced risk of token leakage. Built using <strong>Django Rest Framework</strong>, <strong>MySQL</strong>, and <strong>Django Simple JWT</strong>. Enabled secure integration with other applications through <strong>Coupled Authentication</strong>, utilizing redirect URIs and URL parameters.`
 		},
 		{
 			id: uuidv4(),
-			name: `<a href="https://github.com/Abdul-Mueed-Shahbaz/Secure_Channels-Hub">Authenticated Websockets Server</a>`,
-			desc: `Developed a robust <strong>real-time communication system</strong> using <strong>Django Channels</strong> and <strong>Vue.js</strong>. Features include secure <strong>JWT authentication</strong>, <strong>RESTful user authentication APIs</strong>, <strong>WebSocket integration</strong>, proactive <strong>heartbeat functionality</strong>, and <strong>Redis</strong> for caching and session management.`
+			name: `<a href="https://github.com/abdul-mueed-shz/AuthoBlogs">AuthoBlogs</a>`,
+			desc: `Leveraged <strong>Spring Boot</strong>, <strong>Spring Security</strong>, and <strong>MySQL</strong> for secure blog management. Features include <strong>user authentication</strong>, <strong>CRUD operations</strong>, and security-driven architecture with <strong>Spring Security</strong>. This solution ensures a seamless and secure blogging experience.`
 		},
 		{
 			id: uuidv4(),
 			name: `<a href="https://github.com/Abdul-Mueed-Shahbaz/GlowBeyond">GlowBeyond</a>`,
-			desc: `Developed a <strong>robust</strong> e-commerce platform, <strong>GlowBeyond</strong>, with <strong>Vue.js</strong> frontend and <strong>Django Rest Framework</strong> backend, integrating with <strong>auth-application</strong> for <strong>secure login</strong> and <strong>JWT token authentication</strong>. Features include showcasing <strong>latest products</strong>, categorization, detailed product pages with <strong>add-to-cart functionality</strong>, <strong>cart management</strong>, and <strong>notifications</strong>.`
+			desc: `Developed a robust e-commerce platform, <strong>GlowBeyond</strong>, using a <strong>Vue.js</strong> frontend and <strong>Django Rest Framework</strong> backend. Integrated with an <strong>auth-application</strong> for secure login and <strong>JWT token authentication</strong>. Key features include product showcasing, categorization, detailed product pages with <strong>add-to-cart functionality</strong>, cart management, and notifications.`
 		}
 	],
 	accomplishments: [
