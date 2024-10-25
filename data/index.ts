f7bd18b3212b9beb04846ad7b1a9f1f19cd3cbaa
Äbdul Mueed Shahbaz <abdulmueedshahbaz@gmail.com>
2024-10-26 00:18:32 +0500

    minor changes

--- data/index.ts
+++ data/index.ts
@@ -63,11 +63,11 @@ const userData: IData = {
 			location: 'Lahore, Pakistan | On-Site',
 			duration: 'Aug 2022 - Present',
 			tasks: [
-				`Led the development of backend services using <strong>Java</strong> and <strong>Spring Boot</strong>, creating reusable, scalable microservices that enhanced system reliability and optimized resource usage.`,
-				`Implemented design patterns and best practices to develop robust, reusable Java-based APIs, streamlining data processing and reducing response times by 30%.`,
-				`Architected and managed RESTful APIs using Spring Boot, integrating with databases and external services to provide seamless end-user experiences.`,
+				`Led the development of backend services using <strong>Java</strong> and <strong>Spring Boot</strong>, creating reusable, scalable microservices and integrating <strong>Kafka</strong> queues for asynchronous communication between microservices, handling tasks such as notifications, data synchronization, and processing API requests across different services.`,
+				`Implemented design patterns and best practices to build robust, reusable Java-based APIs, streamlining data processing and reducing response times by <strong>30%</strong>.`,
+				`Architected and managed RESTful APIs with Spring Boot, integrating databases, Kafka, and external services for seamless user experiences and reliable data flow across the microservice architecture.`,
 				`Collaborated with cross-functional teams to define requirements and refine project deliverables, ensuring alignment with business objectives and technical feasibility.`,
-				`Utilized Docker to containerize and orchestrate Java services, supporting scalable deployment and enhancing application stability.`
+				`Utilized Docker to containerize and orchestrate Java services, supporting scalable deployment and improving application stability.`
 			]
 		},
 		{
@@ -78,10 +78,10 @@ const userData: IData = {
 			location: 'San Jose, USA | Remote',
 			duration: 'June 2021 - Aug 2022',
 			tasks: [
-				`Implemented <strong>Refresh Token Rotation based Authentication</strong> for enhanced API security in Django Rest Framework, demonstrating commitment to data confidentiality and reducing the risk of data breaches.`,
-				`Integrated APIs from Google, Facebook, and Instagram into the platform to enhance influencer <strong>Profile Ranking Algorithms</strong>, thereby providing brands with <strong>Advanced Analytics</strong> on influencer reach and engagement.`,
-				`Designed reusable UI components in Vue.js with the <strong>Decorator Pattern</strong>, improving code maintainability and development efficiency across multiple projects.`,
-				`Enhanced the application by implementing robust Database-based, Text-based, and Request <strong>Logging</strong> mechanisms to mitigate developmental errors and streamline production debugging processes.`
+				`Implemented <strong>Refresh Token Rotation Authentication</strong> in Django Rest Framework, improving API security and reducing data breach risks.`,
+				`Integrated APIs from Google, Facebook, and Instagram to enhance influencer <strong>Profile Ranking Algorithms</strong> and provide <strong>Advanced Analytics</strong>.`,
+				`Developed reusable Vue.js UI components using the <strong>Decorator Pattern</strong>, improving maintainability across projects.`,
+				`Enhanced the platform with Database, Text, and Request <strong>Logging</strong> mechanisms, streamlining debugging and error mitigation.`
 			]
 		},
 		{
@@ -92,9 +92,9 @@ const userData: IData = {
 			location: 'Lahore, Pakistan | Hybrid',
 			duration: 'July 2021 - Sep 2021',
 			tasks: [
-				`Innovated game mechanics with a dynamic power-up system, introducing 5 distinct abilities like <strong>Speed Boosts</strong>, and <strong>Shields</strong>, elevating gameplay depth and engagement.`,
-				`Implemented comprehensive solutions for <strong>20</strong> environmental challenges and rectified <strong>10</strong> UI bugs, refining user interaction and level design in the runner game.`,
-				`Enhanced the game's mechanics by refining running, jumping, and animation systems, resulting in a seamless gaming experience and heightened player satisfaction.`
+				`Developed a dynamic power-up system with abilities like <strong>Speed Boosts</strong> and <strong>Shields</strong>, enhancing gameplay depth.`,
+				`Resolved <strong>20</strong> environmental challenges and fixed <strong>10</strong> UI bugs, improving user interaction and level design.`,
+				`Refined running, jumping, and animation mechanics for a smoother gaming experience.`
 			]
 		}
 	],
@@ -119,8 +119,7 @@ const userData: IData = {
 
 	accomplishments: [
 		`Demonstrated Java proficiency through <a href="https://www.coursera.org/account/accomplishments/verify/FX2DYFPCRJPS">Introduction to Java</a> certification, showcasing foundational skills and a commitment to Java development.`,
-		`Mastered React intricacies with <a href="https://coursera.org/share/96a67932b7b57cd7131287028cb5aae1">Meta-React Advanced</a> certification.`,
-		`Acquired foundational knowledge of React with <a href="https://www.coursera.org/account/accomplishments/verify/FJ2S3UCQ65RN?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course">Meta-React Basics</a> certification.`,
+		`Mastered React intricacies and fundamentals through the <a href="https://coursera.org/share/96a67932b7b57cd7131287028cb5aae1">Meta-React Advanced</a> and <a href="https://www.coursera.org/account/accomplishments/verify/FJ2S3UCQ65RN?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course">Meta-React Basics</a> certifications.`,
 		`Established a notable presence on <a href="https://github.com/Abdul-Mueed-Shahbaz">GitHub</a> with diverse JavaScript technology contributions, collectively amassing over <strong>94</strong> stars for my projects.`
 	]
 };
